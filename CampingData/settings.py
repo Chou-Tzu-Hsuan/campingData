f22878fde72fca126c7f1b71df7bbd9cb9c2c24f
Chou TzuHsuan <jojo.chou3@gmail.com>
2025-07-12 01:01:27 +0800

    add settings and update requirements.txt

--- CampingData/settings.py
+++ CampingData/settings.py
@@ -52,6 +52,7 @@ LOGIN_URL = '/userlogin/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,7 @@ DATABASES["default"] = dj_database_url.parse(database_url)
 #---------產品圖片指定路徑---------
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #---------產品圖片指定路徑---------
 
 
@@ -160,7 +162,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
